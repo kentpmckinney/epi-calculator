d38f15d5e3c9aeb475d579674769cf9a637f8422
Kent McKinney <kent.p.mckinney@gmail.com>
2020-01-24 13:40:49 -0800

    add calculation engine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,44 @@
-// var add = function(number1, number2) {
-//     return number1 + number2;
-//   };
-  
-//   var number1 = parseInt(prompt("Enter a number:"));
-//   var number2 = parseInt(prompt("Enter another number:"));
-//   var result = add(number1, number2);
-//   alert(result);
+let math = {
+  '/': function (x, y) { return parseFloat(x) / parseFloat(y) },
+  '*': function (x, y) { return parseFloat(x) * parseFloat(y) },
+  '+': function (x, y) { return parseFloat(x) + parseFloat(y) },
+  '-': function (x, y) { return parseFloat(x) - parseFloat(y) }
+};
 
+function calculate() {
+  // Order of operations = brackets, exponents, division, multiplication, addition, subtraction
+  while (/[^\.\d]+/g.test($("#display").val())) {
+    ["/","*","+","-"].forEach(function(operator) {
+      let str = $("#display").val();
+      let re = new RegExp("[\\d\\.]+(?:\\" + operator + ")[\\d\\.]+");
+      let divMatch = str.match(re)
+      if (divMatch != null) { 
+        let divValues = divMatch.toString().split(operator)
+        let divResult = math[operator](divValues[0],divValues[1]);
+        $("#display").val(str.replace(divMatch, divResult));
+      }
+    });
+  }
+}
 
 $(document).ready(function() {
   
+  $("#display").keyup(function() {
+    $("#display").val($("#display").val().replace(/\D\D|[^/*0-9+-\.]/g,''));
+  });
+
+  $("button").click(function() {
+    let btnValue = $(this).attr("value")
+    switch (btnValue) {
+      case "C":
+        break;
+      case "=":
+          calculate()
+        break;
+      default:
+        $("#display").val($("#display").val() + btnValue);
+    }
+    $("display").focus();
+  });
+
 });
