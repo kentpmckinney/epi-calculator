f638f323a5273e7876e7d56828e2d83fc8ad58b9
Kent McKinney <kent.p.mckinney@gmail.com>
2020-01-25 16:59:00 -0800

    make calculation code easier to read

--- js/script.js
+++ js/script.js
@@ -2,39 +2,42 @@ let math = {
   '/': function (x, y) { return parseFloat(x) / parseFloat(y) },
   '*': function (x, y) { return parseFloat(x) * parseFloat(y) },
   '+': function (x, y) { return parseFloat(x) + parseFloat(y) },
-  '-': function (x, y) { return parseFloat(x) - parseFloat(y) }
+  '-': function (x, y) { return parseFloat(x) - parseFloat(y) },
+  '%': function (x, y) { return parseFloat(x) % parseFloat(y) },
+  '^': function (x, y) { return parseFloat(x) ** parseFloat(y) }
 };
 
 function calculate() {
-  // Order of operations = brackets, exponents, division, multiplication, addition, subtraction
-  while (/[*+/]+|\d+(?:-)\d+/.test($("#display").val())) {
-    let ops = ["/","*","+","-"];
+
+  let iterations = 0;
+  let opsPresent = /[*+/%()\^]+|\d+(?:-)\d+/;
+  while (opsPresent.test($("#display").val())) {
+    if (iterations++ > 10) break; // FOR DEBUGGING - Break out of any unexpected infinite loops
+    let parensPresent = /\(.*\)/g;
+    let parenMatch = $("#display").val().match(parensPresent);
+    let ops = ["^","%","/","*","+","-"]; // Order of operations = paren, exp, div, mul, add, sub
     for (let op of ops) {
-      let str = $("#display").val();
-      let re = new RegExp("[\\d\\.]+(?:\\" + op + ")[\\d\\.]+");
-      let divMatch = str.match(re);
-      let replacement = false;
-      if (divMatch != null) {
-        let divValues = divMatch.toString().split(op)
-        let divResult = math[op](divValues[0],divValues[1]);
-        $("#display").val(str.replace(divMatch, divResult));
-        break; // Start back at the beginning of the while loop to maintain order of operations
+      let str = parenMatch ? parenMatch.toString() : $("#display").val();
+      let matchOpPattern = new RegExp("[\\d\\.]+(?:\\" + op + ")[\\d\\.]+");
+      let match = str.match(matchOpPattern);
+      if (match != null) {
+        let values = match.toString().split(op);
+        let result = math[op](values[0],values[1]);
+        $("#display").val(str.replace(parenMatch ? '(' + match.toString() + ')' : match, result));
+        break; // Start loop over
       }
     }
   }
+
 }
 
 $(document).ready(function() {
   
   $("#display").keyup(function() {
-
-    $("#display").val($("#display").val().replace(/\D\D|[^/*0-9+-\.]/g,''));
-
+    let invalidChracters = /\D[^\d\^]|[^/*0-9+-.%\(\)\^]/g;
+    $("#display").val($("#display").val().replace(invalidChracters,''));
     var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-      calculate(); 
-    }
-
+    if(keycode == '13') calculate(); 
   });
 
   $("button").click(function() {
