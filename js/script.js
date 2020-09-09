fa811d6852c6e12378ed3e7860437a4cf365452e
Kent McKinney <kent.p.mckinney@gmail.com>
2020-09-08 20:47:10 -0700

    enhance user interface and business logic

--- js/script.js
+++ js/script.js
@@ -1,46 +1,74 @@
 function calculate() {
+
   const input = $("#display").val();
-  const output = eval(input);
-  $("#display").val(output);
+  let output = input;
+  try {
+    const result = eval(input);
+    output = result;
+  }
+  catch (e) {
+    showError('Calculation error. Please check the syntax and try again!');
+    return;
+  }
+  finally {
+    $("#display").val(output);
+  }
+
 }
 
-$(document).ready(function () {
+function showError(message) {
 
-  $('#display').bind('keypress', function (e) {
+  $('#error').text(message);
+  $('#error').show();
+
+}
+
+function clearError() {
+
+  $('#error').text('');
+  $('#error').hide();
+
+}
+
+$(document).ready( () => {
+
+  $('#display').bind('keypress', e => {
+
     let event = e || window.event;
     let code = event.keyCode || event.which;
     let key = String.fromCharCode(code);
 
-    if (code === 13) { calculate() }
-
-    // Filter out keys that are always invalid
-    let validCharacters = /[^^/*0-9+-.%]/;
-    if (validCharacters.test(key)) {
-      e.preventDefault();
-      return;
+    const ENTER_KEY = 13;
+    if (code === ENTER_KEY) {
+      clearError();
+      calculate()
     }
 
-    // Filter keys that are invalid in certain contexts
-    let string = $('#display').val();
     let valid = /[^^/*0-9+-.%]/;
-    if (valid.test(string)) {
+    if (valid.test(key)) {
       e.preventDefault();
       return;
     }
 
   });
 
-  $("button").click(function () {
+  $("button").click(function() {
+
     let input = $("#display");
     let btnValue = $(this).attr("value")
     switch (btnValue) {
-      case "C": input.val('');
+      case "C":
+        input.val('');
         break;
-      case "=": calculate()
+      case "=":
+        clearError();
+        calculate();
         break;
       default: input.val(input.val() + btnValue);
     }
     input.focus();
+
   });
 
+  $('#display').focus();
 });
